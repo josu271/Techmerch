682e5cf06ab15b7e0aa5dd3c31ca52d5339fd601
josu271 <jose_enero2004@hotmail.com>
2025-10-03 09:36:47 -0500

    first commit

---
+++ src/main/java/com/example/Techmerch/TechmerchApplication.java
@@ -0,0 +1,13 @@
+package com.example.Techmerch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmerchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmerchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Techmerch/TechmerchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Techmerch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmerchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
